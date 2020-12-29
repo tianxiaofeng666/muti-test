933e57c6cc23a45507f3abe3a47049033eb48118
iyb-tianxiaofeng <tianxiaofeng@iyunbao.com>
2020-12-29 15:52:23 +0800

    测试git提交

---
+++ spring-boot-git/src/main/java/com/example/springbootgit/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.springbootgit.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test(){
+        return "进来了。";
+    }
+}
+
